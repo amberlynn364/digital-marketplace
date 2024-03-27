4d52aa4c4dc445b0c53b1219b682892993634638
amberlynn <amberlynn364@gmail.com>
2024-03-27 22:22:24 +0300

    feat: implement Navbar component

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -17,3 +17,48 @@ export const perks = [
     description: "We've pledged 1% of sales to the preservation and restoration of the natural environment",
   },
 ] as const;
+
+export const productCategories = [
+  {
+    label: 'UI Kits',
+    value: 'ui_kits' as const,
+    featured: [
+      {
+        name: 'Editor picks',
+        href: '#',
+        imageSrc: '/nav/ui-kits/mixed.jpg',
+      },
+      {
+        name: 'New Arrivals',
+        href: '#',
+        imageSrc: '/nav/ui-kits/blue.jpg',
+      },
+      {
+        name: 'Bestsellers',
+        href: '#',
+        imageSrc: '/nav/ui-kits/purple.jpg',
+      },
+    ],
+  },
+  {
+    label: 'Icons',
+    value: 'icons' as const,
+    featured: [
+      {
+        name: 'Favorite Icon Picks',
+        href: '#',
+        imageSrc: '/nav/icons/picks.jpg',
+      },
+      {
+        name: 'New Arrivals',
+        href: '#',
+        imageSrc: '/nav/icons/new.jpg',
+      },
+      {
+        name: 'Bestselling Icons',
+        href: '#',
+        imageSrc: '/nav/icons/bestsellers.jpg',
+      },
+    ],
+  },
+];
