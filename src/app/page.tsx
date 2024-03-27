99a6815bde9a576913df06440eaf8d82310c2e75
amberlynn <amberlynn364@gmail.com>
2024-03-27 20:21:14 +0300

    feat: update Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import HeroSection from '@/components/sections/HeroSection';
+import PerksSection from '@/components/sections/PerksSection';
+
 export default function Home() {
   return (
-    <p>Hello world</p>
+    <>
+      <HeroSection />
+      <PerksSection />
+    </>
   );
 }
